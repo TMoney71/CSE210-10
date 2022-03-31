d89c0f159798c0cd0a680f22d5418e3fd92bed6e
TMoney71 <97584612+TMoney71@users.noreply.github.com>
2022-03-31 16:35:59 -0700

    Update Control_actors_action.py
    reverting the changes

--- Snake/Game/Scripting/Control_actors_action.py
+++ Snake/Game/Scripting/Control_actors_action.py
@@ -1,118 +1,77 @@
 import constants
-from game.casting.actor import Actor
-from game.scripting.action import Action
-from game.shared.point import Point
+from Game.Scripting.Action import Action
+from Game.Shared.Point import Point
 
 
-class HandleCollisionsAction(Action):
+class ControlActorsAction(Action):
     """
-    An update action that handles interactions between the actors.
-    The responsibility of HandleCollisionsAction is to handle the situation when the snake collides
-    with the food, or the snake collides with its segments, or the game is over.
+    An input action that controls the snake.
+    
+    The responsibility of ControlActorsAction is to get the direction and move the snake's head.
     Attributes:
-        _is_game_over (boolean): Whether or not the game is over.
+        _keyboard_service (KeyboardService): An instance of KeyboardService.
     """
 
-    def __init__(self):
-        """Constructs a new HandleCollisionsAction."""
+    def __init__(self, keyboard_service):
+        """Constructs a new ControlActorsAction using the specified KeyboardService.
+        
+        Args:
+            keyboard_service (KeyboardService): An instance of KeyboardService.
+        """
+        self._keyboard_service = keyboard_service
+        self._direction1 = Point(0, constants.CELL_SIZE)
+        self._direction2 = Point(0, -1 * constants.CELL_SIZE)
         self._is_game_over = False
 
     def execute(self, cast, is_game_over, script):
-        """Executes the handle collisions action.
+        """Executes the control actors action.
         Args:
             cast (Cast): The cast of Actors in the game.
             script (Script): The script of Actions in the game.
         """
-        if not self._is_game_over:
-            self._handle_food_collision(cast)
-            self._handle_segment_collision(cast)
-            self._handle_game_over(cast)
-
-    def _handle_food_collision(self, cast):
-        """Updates the score nd moves the food if the snake collides with the food.
-        Args:
-            cast (Cast): The cast of Actors in the game.
-        """
-        score = cast.get_first_actor("scores")
-        food = cast.get_first_actor("foods")
-        cycles = cast.get_actors("cycles")
-        if not self._is_game_over:
-            cycle1 = cycles[0]
-            cycle2 = cycles[1]
-        head = cycle1.get_segments()[0]
-        head2 = cycle2.get_segments()[0]
-
-        if head.get_position().equals(food.get_position()):
-            points = food.get_points()
-            score.add_points(points)
-            food.reset()
-        elif head2.get_position().equals(food.get_position()):
-            points = food.get_points()
-            score.add_points(points)
-            food.reset()
-
-    def _handle_segment_collision(self, cast):
-        """Sets the game over flag if the snake collides with one of its segments.
-        Args:
-            cast (Cast): The cast of Actors in the game.
-        """
+        #probably break this into another method
 
         cycles = cast.get_actors("cycles")
+
+        # left
+        if self._keyboard_service.is_key_down('a'):
+            self._direction1 = Point(-constants.CELL_SIZE, 0)
+
+        # right
+        if self._keyboard_service.is_key_down('d'):
+            self._direction1 = Point(constants.CELL_SIZE, 0)
+
+        # up
+        if self._keyboard_service.is_key_down('w'):
+            self._direction1 = Point(0, -constants.CELL_SIZE)
+
+        # down
+        if self._keyboard_service.is_key_down('s'):
+            self._direction1 = Point(0, constants.CELL_SIZE)
+
         cycle1 = cycles[0]
-        head = cycle1.get_segments()[0]
-        segments = cycle1.get_segments()[1:]
+        cycle1.turn_head(self._direction1)
+
+        cycles = cast.get_actors("cycles")
+
+        # left
+        if self._keyboard_service.is_key_down('j'):
+            self._direction2 = Point(-constants.CELL_SIZE, 0)
+
+        # right
+        if self._keyboard_service.is_key_down('l'):
+            self._direction2 = Point(constants.CELL_SIZE, 0)
+
+        # up
+        if self._keyboard_service.is_key_down('i'):
+            self._direction2 = Point(0, -constants.CELL_SIZE)
+
+        # down
+        if self._keyboard_service.is_key_down('k'):
+            self._direction2 = Point(0, constants.CELL_SIZE)
 
         cycle2 = cycles[1]
-        head2 = cycle2.get_segments()[0]
-        segments2 = cycle2.get_segments()[1:]
+        cycle2.turn_head(self._direction2)
 
-        for segment in segments:
-            if head.get_position().equals(segment.get_position()):
-                self._is_game_over = True
-            for segment in segments2:
-                if head.get_position().equals(segment.get_position()):
-                    self._is_game_over = True
-
-        for segment in segments2:
-            if head2.get_position().equals(segment.get_position()):
-                self._is_game_over = True
-            for segment in segments:
-                if head2.get_position().equals(segment.get_position()):
-                    self._is_game_over = True
-
-    def _handle_game_over(self, cast):
-        """Shows the 'game over' message and turns the snake and food white if the game is over.
-        Args:
-            cast (Cast): The cast of Actors in the game.
-        """
-        if self._is_game_over:
-            cycles = cast.get_actors("cycles")
-            cycle1 = cycles[0]
-            segments = cycle1.get_segments()
-            food = cast.get_first_actor("foods")
-
-            cycle2 = cycles[1]
-            segments2 = cycle2.get_segments()
-
-            x = int(constants.MAX_X / 2)
-            y = int(constants.MAX_Y / 2)
-            position = Point(x, y)
-
-            message = Actor()
-            message.set_text("Game Over!")
-            message.set_position(position)
-            cast.add_actor("messages", message)
-
-            for segment in segments:
-                 segment.set_color(constants.WHITE)
-            for segment in segments2:
-                 segment.set_color(constants.WHITE)
-            food.set_color(constants.WHITE)
-
-    def get_is_game_over(self):
-        return self._is_game_over
-
-from game.scripting.action import Action
-from game.shared.point import Point
-
-	
+    def set_is_game_over(self, is_game_over):
+        self._is_game_over = is_game_over
