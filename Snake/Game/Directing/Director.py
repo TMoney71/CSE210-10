e36f021d0ab5f40febff23603dab36f5807e6787
TMoney71 <97584612+TMoney71@users.noreply.github.com>
2022-03-31 16:19:17 -0700

    Update Director.py
    Moving Juan's code over

--- Snake/Game/Directing/Director.py
+++ Snake/Game/Directing/Director.py
@@ -1,24 +1,47 @@
-# This will be the Director file
-from Game.Services.Video_service import VideoService
-from Game.Casting.Cast import Cast
-from Game.Scripting.Script import Script
+from game.scripting.handle_collisions_action import HandleCollisionsAction
+
 
 class Director:
-    def __init__(self, video_service):
+     """A person who directs the game. 
     
+    The responsibility of a Director is to control the sequence of play.
+    Attributes:
+        _video_service (VideoService): For providing video output.
+    """
+        
+   def __init__(self, video_service):
+        """Constructs a new Director using the specified video service.
+        
+        Args:
+            video_service (VideoService): An instance of VideoService.
+        """
         self._video_service = video_service
-    def start_game(self, cast, script):
+        self._is_game_over = False
 
-
+     def start_game(self, cast, script):
+        """Starts the game using the given cast and script. Runs the main game loop.
+        Args:
+            cast (Cast): The cast of actors.
+            script (Script): The script of actions.
+        """
         self._video_service.open_window()
         while self._video_service.is_window_open():
             self._execute_actions("input", cast, script)
-            self._execute_actions("update", cast, script)
-            self._execute_actions("output", cast, script)
-        self._video_service.close_window()
-
+            
+            self._video_service.close_window()
+            
+            
     def _execute_actions(self, group, cast, script):
+        """Calls execute for each action in the given group.
         
-        actions = script.get_actions(group)    
+        Args:
+            group (string): The action group name.
+            cast (Cast): The cast of actors.
+            script (Script): The script of actions.
+        """
+        actions = script.get_actions(group)
         for action in actions:
-            action.execute(cast, script)     
+            action.execute(cast, self._is_game_over, script)
+
+            if isinstance(action, HandleCollisionsAction):
+                self._is_game_over = action.get_is_game_over()
